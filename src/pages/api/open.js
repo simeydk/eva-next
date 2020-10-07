63355956729bec6f3862bc1c2707593bb03e17d9
Simey de Klerk <simeydeklerk@gmail.com>
2020-10-08 00:20:10 +0200

    add open route

---
+++ src/pages/api/open.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+const open = require('open')
+
+export default async (req, res) => {
+    try {
+        const filePath = req.query.p
+        await open(filePath)
+        res.statusCode = 200
+        res.json({ status: `succesfully opened`, path: filePath })
+    } catch (error) {
+        res.statusCode = 500
+        res.json({error})
+    }
+}
