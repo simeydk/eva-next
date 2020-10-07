7f9963215e4082f569a2111cb5eb8333a9f84a97
Simey de Klerk <simeydeklerk@gmail.com>
2020-10-07 23:53:30 +0200

    Add caching to icon fetcher

--- src/pages/api/icon.js
+++ src/pages/api/icon.js
@@ -1,17 +1,36 @@
 const fs = require('fs')
-const { getImageForPath } = require('shell-image-win');
+const path = require('path')
+const { getImageForPath, flags } = require('shell-image-win');
+const cloneBuffer = require('clone-buffer')
 
-export default (req, res) => {
-  const path = req.query.for
-  const size = Number(req.query.size) | 32 
-  
+const ICON_FOLDER = path.join('.data', 'ICONS')
+
+async function getPngIconBuffer(filePath, size = 32) {
+  const ext = path.extname(filePath).toLowerCase()
+  const iconName = path.join(ICON_FOLDER, `${ext}_${size}.PNG`)
+  if (fs.existsSync(iconName)) {
+    // console.log('from cache', {filePath, size, ext, iconName})
+    return fs.promises.readFile(iconName)
+  } else {
+    // console.log('fresh', {filePath, size, ext, iconName})
+    const imageBuffer = await getImageForPath(filePath, { height: size, width: size, flags: flags.IconOnly })
+    fs.mkdirSync(ICON_FOLDER, { recursive: true })
+    fs.promises.writeFile(iconName, cloneBuffer(imageBuffer))
+    return imageBuffer
+  }
+}
+
+export default async (req, res) => {
+  const filePath = req.query.for
+  const size = Number(req.query.size) | 32
   try {
-    getImageForPath(path, { height: size, width: size }, (err, imageBuffer) => {      
-        res.setHeader('Content-Type', 'image/png')
-        res.send(imageBuffer)
-        res.end()
-      })
-  } catch {
-      res.end()
+    const buffer = await getPngIconBuffer(filePath, size)
+    res.setHeader('Content-Type', 'image/png')
+    await res.send(buffer)
+    res.end()
+  } catch (error) {
+    console.log({error, filePath, size})
+    res.send('error')
+    res.end()
   }
 }
