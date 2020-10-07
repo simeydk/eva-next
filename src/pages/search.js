e4102764e62ab115c291d55005bd87e3ddc9515f
Simey de Klerk <simeydeklerk@gmail.com>
2020-10-07 23:53:05 +0200

    link up search component

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,14 +1,38 @@
 
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import path from 'path'
+import {debounce} from 'lodash'
 
-export default function Search({ time = "time" }) {
+function useFetch(url, initialvalue = '') {
+  const [response, setResponse] = useState(initialvalue)
+  useEffect(() => {fetch(url).then(response => response.json()).then(setResponse)}, [url])
+  return response
+}
+
+function useDraft(initialValue) {
+  const [draft, setDraft] = useState(initialValue)
+  const [value, setValue] = useState(initialValue)
+  const commit = () => setValue(draft)
+  const d = debounce(commit,300)
+  useEffect(() => {d()},[draft])
+  return [draft, setDraft, value, commit]
+}
+
+export default function Search() {
+  const [draft, setDraft, query, commit] = useDraft('variable xls')
+  const response = useFetch(`/api/search?q=${query}`,{q:query,results:[]})
+
+  const debouncedCommit = debounce(commit,600)
+  const onChange = async e => {
+    await setDraft(e.target.value)
+  }
+
   return (
     <div className="min-h-screen bg-gray-200 p-1">
-      <form className="text-xl mx-auto max-w-2xl m-4 p-4 bg-gradient-to-b from-gray-500 to-gray-700 rounded-lg shadow-lg">
+      <form onSubmit={e => {e.preventDefault(); commit()} } className="text-xl mx-auto max-w-2xl m-4 p-4 bg-gradient-to-b from-gray-500 to-gray-700 rounded-lg shadow-lg">
 
         <div className="relative">
-          <input type="text" value="archer" className="w-full text-xl py-1 px-3 rounded-lg bg-gray-300 text-gray-700 border border-gray-500 focus:outline-none focus:bg-white focus:text-gray-800 focus:border-teal-800 transition-colors duration-100" />
+          <input type="text" value={draft} onChange={onChange} className="w-full text-xl py-1 px-3 rounded-lg bg-gray-300 text-gray-700 border border-gray-500 focus:outline-none focus:bg-white focus:text-gray-800 focus:border-teal-800 transition-colors duration-100" />
         </div>
 
       </form>
@@ -35,14 +59,8 @@ export default function Search({ time = "time" }) {
                 <th className="px-6 py-3 bg-gray-50"></th>
               </tr>
             </thead>
-            <div></div>
             <tbody className="bg-white divide-y divide-gray-200 border border-gray-200  shadow-sm rounded-xl">
-              <Result />
-              <Result />
-              <Result />
-              <Result />
-              <Result />
-
+              {response.results.map(result => <Result {...result}/>)}
             </tbody>
           </table>
         </div>
@@ -54,45 +72,28 @@ export default function Search({ time = "time" }) {
 }
 
 
-const demoFiles = [
-  "C:\\Users\\simey\\OneDrive\\Documents\\Siemens Toastmasters Dues April 2020.docx",
-  "C:\\Users\\simey\\OneDrive\\Documents\\Default.rdp",
-  "C:\\Users\\simey\\OneDrive\\Documents\\Easy-speak made easy 20200716.pptx",
-  "C:\\Users\\simey\\OneDrive\\Documents\\OMF Credit Life NDR Estimate 2020-03 (based on SAM 2020 Q3).xlsx",
-  "C:\\Users\\simey\\OneDrive\\Documents\\OMART.url",
-  "C:\\Users\\simey\\OneDrive\\Documents\\Emma woorde.pptx",
-  "D:\\Ebooks\\Calibre Library\\Dave Cullen\\Columbine (47)\\Columbine - Dave Cullen.mobi",
-  "D:\\Ebooks\\Calibre Library\\Dave Cullen\\Columbine (47)\\cover.jpg",
-  "D:\\Ebooks\\Calibre Library\\Dave Cullen\\Columbine (47)\\metadata.opf",
-]
-
-const randomElement = arr => arr[Math.floor(Math.random() * arr.length)]
-
-function Result() {
-  const [src, setSrc] = useState(randomElement(demoFiles))
-  const filename = path.basename(src)
+function Result({name = '', location = '', fullName = '', sizeBytes = 0, mtime=''}) {
   return <tr>
     <td className="px-3 py-1 whitespace-no-wrap">
       <div className="flex items-center">
         <div className="flex-shrink-0 h-8 w-8">
-          <img className="h-8 w-8" src={`/api/icon?for=${src}`} alt="" />
+          <img className="h-8 w-8" src={`/api/icon?for=${fullName}`} alt="" />
         </div>
         <div className="ml-3">
           <div className="text-sm leading-5 font-medium text-gray-900">
-            {/* Kane and Abel - Jeffrey Archer.mobi */}
-            {filename}
+            {name}
           </div>
           <div className="text-sm leading-5 text-gray-500">
-            D:\Downloads\_Ebooks\Archer, Jeffrey
+            {location}
           </div>
         </div>
       </div>
     </td>
     <td className="px-6 py-4 whitespace-no-wrap">
-      <div className="text-sm leading-5 text-gray-500">14 Sep</div>
+      <div className="text-sm leading-5 text-gray-500">{mtime}</div>
     </td>
     <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-      1.3MB
+      {sizeBytes}
     </td>
     <td className="px-6 py-2 whitespace-no-wrap text-right text-xs leading-5 font-medium">
       <div>
