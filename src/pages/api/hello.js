aab18ef514c11282637c4b618bd09cb832dfba87
Simey de Klerk <simeydeklerk@gmail.com>
2020-10-11 23:13:01 +0200

    tweak hello world endpoint

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,8 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import x from 'node-system-icon'
-
 export default (req, res) => {
-  console.log(req)
+  console.log({req})
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
